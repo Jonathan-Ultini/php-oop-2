64fd5a1af8958af993cdb0d84c9f97d3d067bad4
Jonathan-Ultini <jonathanultini@gmail.com>
2024-10-10 16:18:01 +0200

    add inheritance food class

---
+++ classes/Cibo.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'Prodotto.php';
+
+class Cibo extends Prodotto
+{
+  public $ingredienti;
+
+  public function __construct($titolo, $prezzo, $immagine, Categoria $categoria, $tipo, $ingredienti)
+  {
+    parent::__construct($titolo, $prezzo, $immagine, $categoria, $tipo);
+    $this->ingredienti = $ingredienti;
+  }
+
+  public function displayProduct()
+  {
+    parent::displayProduct();
+    echo "<p>Ingredienti: {$this->ingredienti}</p>";
+  }
+}
--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
 require_once __DIR__ . '/classes/Prodotto.php';
 require_once __DIR__ . '/classes/Categoria.php';
+require_once __DIR__ . '/classes/Cibo.php';
+require_once __DIR__ . '/classes/Gioco.php';
+require_once __DIR__ . '/classes/Cuccia.php';
+
 
 $categoriaCani = new Categoria('Cani');
 $categoriaGatti = new Categoria('Gatti');
 
-$product = new Prodotto('croccantini', 23, 'https://invirtus.it/wp-content/uploads/2021/04/A145A.jpg', $categoriaCani, 'pollo');
+$ciboCani = new Cibo('Croccantini', 23, 'https://invirtus.it/wp-content/uploads/2021/04/A145A.jpg', $categoriaCani, 'Sacchetto', 'Carne di pollo, riso');
 
-$product->displayProduct();
+// $product = new Prodotto('croccantini', 23, 'https://invirtus.it/wp-content/uploads/2021/04/A145A.jpg', $categoriaCani, 'pollo');
+
+// $product->displayProduct();
+$ciboCani->displayProduct();
